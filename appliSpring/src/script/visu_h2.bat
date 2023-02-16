ccb212abc9bf94667bac8df3e6ddca25140c8d1c
DidierDefrance <didier@d-defrance.fr>
2023-02-16 11:30:22 +0100

    visu_h2.bat

--- appliSpring/src/script/visu_h2.bat
+++ appliSpring/src/script/visu_h2.bat
@@ -1,3 +1,5 @@
 set JAVA_HOME=C:\Program Files\Java\jdk-17
+REM JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
 set H2_PATH=C:\Users\d2fde\.m2\repository\com\h2database\h2\2.1.214
+REM set H2_PATH=C:\Users\Administrateur\.m2\repository\com\h2database\h2\2.1.214
 java -jar %H2_PATH%/h2-2.1.214.jar
