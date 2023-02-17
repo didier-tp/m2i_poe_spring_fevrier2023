0bd70825af737f4cd5062960240b34f35998cd30
DidierDefrance <didier@d-defrance.fr>
2023-02-17 14:56:25 +0100

    s

--- appliSpring/src/main/java/com/m2i/tp/appliSpring/service/CompteServiceImpl.java
+++ appliSpring/src/main/java/com/m2i/tp/appliSpring/service/CompteServiceImpl.java
@@ -11,6 +11,7 @@ import com.m2i.tp.appliSpring.converter.MyGenericMapper;
 import com.m2i.tp.appliSpring.dao.ICompteRepository;
 import com.m2i.tp.appliSpring.dto.CompteDto;
 import com.m2i.tp.appliSpring.entity.Compte;
+import com.m2i.tp.appliSpring.exception.NotFoundException;
 
 
 //@Component
@@ -22,7 +23,6 @@ public class CompteServiceImpl implements ICompteService {
 	@Resource
 	private ICompteRepository compteRepository;//=null par defaut
 	
-	
 
 	@Override
 	public List<CompteDto> findAll() {
@@ -30,8 +30,10 @@ public class CompteServiceImpl implements ICompteService {
 	}
 
 	@Override
-	public CompteDto findById(Integer id) {
+	public CompteDto findById(Integer id) throws NotFoundException{
 		Compte compte = compteRepository.findById(id).orElse(null);
+		if(compte==null)
+			throw new NotFoundException("compte not found with id=" +id);
 		CompteDto compteDto = MyGenericMapper.map(compte, CompteDto.class);
 		//compteDto.setNumero(compte.getId()); //faisable ici ou ailleurs si nécessaire
 		return compteDto;
@@ -44,6 +46,17 @@ public class CompteServiceImpl implements ICompteService {
 		return MyGenericMapper.map(compteEntity,CompteDto.class);
 		//cptDto.setId(compteEntity.getId());
 		//return cptDto;
+	}
+	
+	@Override
+	public CompteDto update(CompteDto cptDto) throws NotFoundException {
+		if(cptDto==null || cptDto.getId()==null) throw new NotFoundException("null not found");
+		if(compteRepository.existsById(cptDto.getId())==false)
+			throw new NotFoundException("impossible de mettre à jour le compte avec id="
+		                                +cptDto.getId()+ " car existe pas");
+		Compte compteEntity = MyGenericMapper.map(cptDto,Compte.class);
+		compteRepository.save(compteEntity);
+		return cptDto;
 	}
 
 	@Override
@@ -63,6 +76,32 @@ public class CompteServiceImpl implements ICompteService {
 			//e.printStackTrace();
 			throw new RuntimeException("echec virement",e);
 		}
+	}
+
+	@Override
+	public List<CompteDto> findBySoldeMini(double soldeMini) {
+		return MyGenericMapper.map(compteRepository.findBySoldeGreaterThanEqual(soldeMini),
+				                   CompteDto.class);
+	}
+
+	@Override
+	public List<CompteDto> findByClientId(Integer idClient) {
+		return MyGenericMapper.map(compteRepository.findByClientId(idClient),
+                                   CompteDto.class);
+	}
+
+	@Override
+	public boolean ifEntityExistById(int id) {
+		return compteRepository.existsById(id);
+	}
+
+	
+
+	@Override
+	public void deleteById(Integer id) throws NotFoundException {
+		if(compteRepository.existsById(id)==false)
+			throw new NotFoundException("impossible de supprimer le compte avec id="+id+ " car existe pas");
+		compteRepository.deleteById(id);
 	}
 
 	
