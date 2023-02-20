ca964fa519b28580488c842fb97a7db79f4557f2
DidierDefrance <didier@d-defrance.fr>
2023-02-20 13:59:45 +0100

    post, put, delete

---
+++ appliSpring/src/main/java/com/m2i/tp/appliSpring/dto/InfoDto.java
@@ -0,0 +1,14 @@
+package com.m2i.tp.appliSpring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @NoArgsConstructor
+@AllArgsConstructor
+public class InfoDto {
+	protected String message;
+	protected String detail;
+}
--- appliSpring/src/main/java/com/m2i/tp/appliSpring/rest/CompteRestCtrl.java
+++ appliSpring/src/main/java/com/m2i/tp/appliSpring/rest/CompteRestCtrl.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m2i.tp.appliSpring.dto.CompteDto;
+import com.m2i.tp.appliSpring.dto.ErrorDto;
+import com.m2i.tp.appliSpring.dto.InfoDto;
+import com.m2i.tp.appliSpring.exception.NotFoundException;
 import com.m2i.tp.appliSpring.service.ICompteServiceV2;
 
 @RestController 
@@ -63,6 +70,29 @@ public class CompteRestCtrl {
 	@PostMapping("")
 	public CompteDto postCompte(@RequestBody CompteDto compteDto) {
 		return compteService.create(compteDto);//id auto incrémenté 
+	}
+	
+	// URL: http://localhost:8080/appliSpring/api-bank/compte
+	//appelé en mode POST avec dans le corps de la requete:
+	//    { "id" : 3 , "label" : "compte_XY" , "solde" : 77.77 }
+	@PutMapping("")
+	public CompteDto putCompte(@RequestBody CompteDto compteDto) {
+		return compteService.update(compteDto);
+	}
+	
+	// URL: http://localhost:8080/appliSpring/api-bank/compte/3
+	//appelé en mode DELETE
+	@DeleteMapping("/{idCpt}")
+    public ResponseEntity<?> deleteCompte(@PathVariable("idCpt") Integer id) {
+		try {
+			compteService.deleteById(id);
+			//return new ResponseEntity<CompteDto>(HttpStatus.NO_CONTENT);//HttpStatus.NO_CONTENT=204
+			return new ResponseEntity<InfoDto>( new InfoDto("compte bien supprimé" , "id="+id) ,
+					                             HttpStatus.OK);//HttpStatus.OK=200
+		} catch (NotFoundException e) {
+			return new ResponseEntity<ErrorDto>( new ErrorDto("compte not found" ,e.getMessage()) ,
+					                            HttpStatus.NOT_FOUND);//404_NOT_FOUND
+		}
 	}
 	
 
