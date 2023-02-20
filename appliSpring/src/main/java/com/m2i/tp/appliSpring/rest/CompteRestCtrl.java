5a431a0e3c9200fe3e5d58ecfc56f9973a878792
DidierDefrance <didier@d-defrance.fr>
2023-02-20 10:40:12 +0100

    get

--- appliSpring/src/main/java/com/m2i/tp/appliSpring/rest/CompteRestCtrl.java
+++ appliSpring/src/main/java/com/m2i/tp/appliSpring/rest/CompteRestCtrl.java
@@ -1,10 +1,13 @@
 package com.m2i.tp.appliSpring.rest;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m2i.tp.appliSpring.dto.CompteDto;
@@ -21,6 +24,17 @@ public class CompteRestCtrl {
 	@GetMapping("/{idCpt}")
 	public CompteDto getCompteById(@PathVariable("idCpt") Integer id) {
 		return compteService.findById(id);
+	}
+	
+	//exemple URL: http://localhost:8080/appliSpring/api-bank/compte
+	//ou bien      http://localhost:8080/appliSpring/api-bank/compte?soldeMini=100
+	@GetMapping("")
+	public List<CompteDto> getCompteByCriteria(
+			@RequestParam(value="soldeMini",required=false)  Double soldeMini) {
+		if(soldeMini==null)
+		    return compteService.findAll();
+		else
+			return compteService.findBySoldeMini(soldeMini);
 	}
 
 	
