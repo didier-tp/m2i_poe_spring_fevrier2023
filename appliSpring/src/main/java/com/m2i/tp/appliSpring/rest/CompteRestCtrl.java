912ae0f77ac977ef4877a2c9bb2eedc28d22ac6d
DidierDefrance <didier@d-defrance.fr>
2023-02-20 14:50:24 +0100

    s

--- appliSpring/src/main/java/com/m2i/tp/appliSpring/rest/CompteRestCtrl.java
+++ appliSpring/src/main/java/com/m2i/tp/appliSpring/rest/CompteRestCtrl.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,8 @@ import com.m2i.tp.appliSpring.exception.NotFoundException;
 import com.m2i.tp.appliSpring.service.ICompteServiceV2;
 
 @RestController 
+@CrossOrigin(origins = "*" , methods = { RequestMethod.GET , RequestMethod.POST , 
+		                                 RequestMethod.PUT , RequestMethod.DELETE })
 @RequestMapping(value="/api-bank/compte" , headers="Accept=application/json")
 public class CompteRestCtrl {
 	
